85e2515c8bbc4663a11f1e46aab88546682cb595
Selva <peratchiselvan@tuta.io>
2018-12-04 13:45:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git clone https://gitlab.com/Peratchiselvan/ArchlinuxDotFiles
-cp ArchlinuxDotFiles/config/i3 ~/.config/i3
-cp ArchlinuxDotFiles/dmenu-frecency ~/dmenu-frecency
+cp -r ArchlinuxDotFiles/config/i3 ~/.config/i3
+cp ArchlinuxDotFiles/scripts/dmenu-frecency ~/dmenu-frecency
+chmod +x ~/dmenu-frecency
 cp ArchlinuxDotFiles/i3status.conf ~/.i3status.conf
 rm -rf ArchlinuxDotFiles
 echo "Configs are updated. Logout and login to i3wm"
